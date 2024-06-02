5852473970a3a52aac91d62c52aa63d9f95af9e5
Sabeel Khan <sabeelhps@gmail.com>
2024-06-02 13:42:58 +0530

    React Intro

---
+++ Class-16/food-app-v1/client/src/components/Food/Food.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const Food = ({name, price, desc, imageUrl}) => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+    <CardMedia
+      sx={{ height: 240 }}
+      image={imageUrl}
+      title="green iguana"
+    />
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="div">
+        {name}
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        {desc}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Buy Now</Button>
+      <Button size="small">Add To Cart</Button>
+    </CardActions>
+  </Card>
+  )
+}
+
+export default Food;
+
